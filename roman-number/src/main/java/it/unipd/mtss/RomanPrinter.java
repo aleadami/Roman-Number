729620c11ad4031f03b649729d72baedceed8801
aleadami <alessandro.adami2003@gmail.com>
2026-05-04 11:13:08 +0200

    modifica RomanPrinter per i primi 3 numeri

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -16,67 +16,31 @@ public class RomanPrinter {
         String[][] asciiLetters = {
             {
                 //Riga1
-                "  __  __ ", // M
-                "  _____  ",  // D
-                "   _____ ",  // C
-                "  _      ",  // L
-                " __   __",  // X
-                " __      __",  // V
                 "  _____ " // I
             },
 
             {
                 //Riga2
-                " |  \\/  |", // M
-                " |  __ \\ ",  // D
-                "  / ____|",  // C
-                " | |     ",  // L
-                " \\ \\ / /",  // X
-                " \\ \\    / /",  // V
                 " |_   _|" // I
             },
 
             {
                 //Riga3
-                " | \\  / |", // M
-                " | |  | |",  // D
-                " | |     ",  // C
-                " | |     ",  // L
-                "  \\ V / ",  // X
-                "  \\ \\  / / ",  // V
                 "   | |  " // I
             },
 
             {
                 //Riga4
-                " | |\\/| |", // M
-                " | |  | |", // D
-                " | |     ", // C
-                " | |     ", // L
-                "   > <  ", // X
-                "   \\ \\/ /  ", // V
                 "   | |  " // I
             },
                 
             {
                 //Riga5
-                " | |  | |", // M
-                " | |__| |", // D
-                " | |____ ", // C
-                " | |____ ", // L
-                "  / . \\ ", // X
-                "    \\  /   ", // V
                 "  _| |_ " // I
             },
                 
             {
                 //Riga6
-                " |_|  |_|", // M
-                " |_____/ ", // D
-                "  \\_____|", // C
-                " |______|", // L
-                " /_/ \\_\\", // X
-                "     \\/    ", // V
                 " |_____|" // I
             },
         };
@@ -84,7 +48,7 @@ public class RomanPrinter {
         for( int z=0; z<6; z++) {
             for (char c : romanNumber.toCharArray()) {
                 // Aggiungi la rappresentazione ASCII art orizzontale della lettera corrente
-                asciiArt.append(asciiLetters[z]["MDCLXVI".indexOf(c)]);
+                asciiArt.append(asciiLetters[z]["I".indexOf(c)]);
             }
             asciiArt.append("\n");
         }
