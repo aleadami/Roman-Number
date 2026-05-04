a3d787e4f77d3fbf894e1fe45f800dc1a22eeb5b
aleadami <alessandro.adami2003@gmail.com>
2026-05-04 11:27:19 +0200

    implementazione classe RomanPrinterTest

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -11,5 +11,8 @@ class RomanPrinterTest {
     @Test
     void smokeTest() {
         // Metodo vuoto: non testa nulla ma fa generare i report
+        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
+        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
+        assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
     }
 }
